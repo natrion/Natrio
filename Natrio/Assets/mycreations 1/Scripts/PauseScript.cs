768bb2f472c38b2702c51463959e62ce81841746
natrion <109035902+natrion@users.noreply.github.com>
2022-07-27 21:40:12 +0200

    Menu2

---
+++ Natrio/Assets/mycreations 1/Scripts/PauseScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    private bool paused =false;
+    public GameObject PauseMenu;
+    void Start()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {        
+            if (paused == true)
+            {
+                PauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+                paused = false;
+            }else
+            {
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+                paused = true;
+            }            
+        }       
+    }
+    public void UnPaused()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
